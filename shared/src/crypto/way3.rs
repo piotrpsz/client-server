ab0d270940e5e5b0cd92949ffc5821d53ae18ab6
piotrpsz <piotr@beesoft.pl>
2025-04-28 12:30:21 +0200

    3 way

--- shared/src/crypto/way3.rs
+++ shared/src/crypto/way3.rs
@@ -14,7 +14,108 @@ impl Way3 {
         if key.len() != KEY_SIZE {
             return Err("invalid key size");
         }
-        
-        todo!()
+        let mut k = [0u32; 3];
+        let mut ki = [0u32; 3];
+        unsafe {
+            let ptr = key.as_ptr() as *const u32;
+            k[0] = *ptr;
+            k[1] = *(ptr.offset(1));
+            k[2] = *(ptr.offset(2));
+            ki[0] = *ptr;
+            ki[1] = *(ptr.offset(1));
+            ki[2] = *(ptr.offset(2));
+        }
+        Self::mu(Self::theta(&mut ki));
+        Ok(Way3 { k, ki })
     }
+    
+    fn mu(data: &mut [u32]) -> &mut [u32]{
+        let mut a0 = data[0];
+        let mut a1 = data[1];
+        let mut a2 = data[2];
+        let mut b0 = 0u32;
+        let mut b1 = 0u32;
+        let mut b2 = 0u32;
+        
+        for _i in 0..32 {
+            b0 <<= 1;
+            b1 <<= 1;
+            b2 <<= 1;
+            b0 |= a2 & 1;
+            b1 |= a1 & 1;
+            b2 |= a0 & 1;
+            a0 >>= 1;
+            a1 >>= 1;
+            a2 >>= 1;
+        }
+        data[0] = b0;
+        data[1] = b1;
+        data[2] = b2;
+        data
+    }
+    
+    fn theta(data: &mut [u32]) -> &mut [u32]{
+        let a0 = data[0];
+        let a1 = data[1];
+        let a2 = data[2];
+        
+        data[0] = a0 ^
+            (a0 >> 16) ^ (a1 << 16) ^
+            (a1 >> 16) ^ (a2 << 16) ^
+            (a1 >> 24) ^ (a2 <<  8) ^
+            (a2 >>  8) ^ (a0 << 24) ^
+            (a2 >> 16) ^ (a0 << 16) ^
+            (a2 >> 24) ^ (a0 <<  8);
+
+        data[1] = a1 ^
+            (a1 >> 16) ^ (a2 << 16) ^
+            (a2 >> 16) ^ (a0 << 16) ^
+            (a2 >> 24) ^ (a0 <<  8) ^
+            (a0 >>  8) ^ (a1 << 24) ^
+            (a0 >> 16) ^ (a1 << 16) ^
+            (a0 >> 24) ^ (a1 << 8);
+
+        data[2] = a2 ^
+            (a2 >> 16) ^ (a0 << 16) ^
+            (a0 >> 16) ^ (a1 << 16) ^
+            (a0 >> 24) ^ (a1 <<  8) ^
+            (a1 >>  8) ^ (a2 << 24) ^
+            (a1 >> 16) ^ (a2 << 16) ^
+            (a1 >> 24) ^ (a2 << 8);
+        data
+    }
+    
+    fn gamma(data: &mut [u32]) -> &mut [u32]{
+        let a0 = data[0];
+        let a1 = data[1];
+        let a2 = data[2];
+    
+        data[0] = !a0 ^ (!a1 & a2);
+        data[1] = !a1 ^ (!a2 & a0);
+        data[2] = !a2 ^ (!a0 & a1);
+        data
+    }
+    
+    fn pi1(data: &mut [u32]) -> &mut [u32]{
+        let a0 = data[0];
+        let a2 = data[2];
+        
+        data[0] = (a0 >> 10) ^ (a0 << 22);
+        data[2] = (a2 << 1) ^ (a2 >> 31);
+        data
+    }
+    
+    fn pi2(data: &mut [u32]) -> &mut [u32]{
+        let a0 = data[0];
+        let a2 = data[2];
+
+        data[0] = (a0 <<  1) ^ (a0 >> 31);
+        data[2] = (a2 >> 10) ^ (a2 << 22);
+        data       
+    }
+    
+    fn rho(data: &mut [u32]) -> &mut [u32]{
+        Self::pi2(Self::gamma(Self::pi1(Self::theta(data))))    
+    }    
+    
 }

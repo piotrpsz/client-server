ef1aa00ee1b12d6253d9ab12137be6eff09c116e
piotrpsz <piotr@beesoft.pl>
2025-05-07 21:00:36 +0200

    adding license information in the header of each file

--- shared/src/net/connector.rs
+++ shared/src/net/connector.rs
@@ -1,8 +1,25 @@
-// Moduł odpowiedzialny za komunikację poprzez socket.
-// 1. Zarówno serwer, jak i klient znają klucz do Blowfisha.
-// 2. Klient po nawiązaniu połączenia przesyła swój identyfikator (128 bajtów) zaszyfrowany Blowfishem,
-// 3. Jeśli identyfikator jest OK, serwer odsyła hasła dla GOST i 3Wwy (zaszyfrowane Blowfishem).
-// Od tej pory cała komunikacja szyfrowana jest BG3 (szyfr Blowfish - deszyfr GOST - szyfr 3Way3)
+// MIT License
+// 
+// Copyright (c) 2025 Piotr Pszczółkowski
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 
 use std::io;
 use std::io::{Error, ErrorKind};
